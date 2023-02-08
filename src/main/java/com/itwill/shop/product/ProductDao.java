3fff16e6b5bb818a727323c573228ec7b96397b3
kryer1317 <ITWILL@DESKTOP-VGFOO9C>
2023-02-08 15:29:09 +0900

    ..

--- src/main/java/com/itwill/shop/product/ProductDao.java
+++ src/main/java/com/itwill/shop/product/ProductDao.java
@@ -32,37 +32,54 @@ public class ProductDao {
 	 */
 	public List<Product> selectByCategory(int category_no) throws Exception {
 		List<Product> productList=new ArrayList<Product>();
-		Connection con=dataSource.getConnection();
-		PreparedStatement pstmt=con.prepareStatement(ProductSQL.PRODUCT_SELECT_BY_CATEGORY);
-		pstmt.setInt(1, category_no);
-		ResultSet rs=pstmt.executeQuery();
-		while(rs.next()) {
-			Product product = new Product(rs.getInt("p_no"), rs.getString("p_name"), rs.getInt("p_price"), 
-					  			rs.getString("p_image"), rs.getString("p_desc"), 
-					  			rs.getInt("p_click_count"), rs.getInt("category_no"));
-			productList.add(product);
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try {
+			con=dataSource.getConnection();
+			pstmt=con.prepareStatement(ProductSQL.PRODUCT_SELECT_BY_CATEGORY);
+			pstmt.setInt(1, category_no);
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+				Product product = new Product(rs.getInt("p_no"), rs.getString("p_name"), rs.getInt("p_price"), 
+						  			rs.getString("p_image"), rs.getString("p_desc"), 
+						  			rs.getInt("p_click_count"), rs.getInt("category_no"));
+				productList.add(product);
+			}
+		}finally {
+			if (rs != null)
+				rs.close();
+			if (pstmt != null)
+				pstmt.close();
+			if (con != null)
+				con.close();
 		}
-		rs.close();
-		pstmt.close();
-		con.close();
 		return productList;
 	}
 	/*
 	 * 상품추가
 	 */
 	public int insert(Product product) throws Exception {
-		Connection con = dataSource.getConnection();
-		PreparedStatement pstmt = con.prepareStatement(ProductSQL.PRODUCT_INSERT);
-		pstmt.setInt(1, product.getP_no());
-		pstmt.setString(2, product.getP_name());
-		pstmt.setInt(3, product.getP_price());
-		pstmt.setString(4, product.getP_image());
-		pstmt.setString(5, product.getP_desc());
-		pstmt.setInt(6, product.getP_click_count());
-		pstmt.setInt(7, product.getCategory_no());
-		int rowCount = pstmt.executeUpdate();
-		pstmt.close();
-		con.close();
+		Connection con = null;
+		PreparedStatement pstmt =null;
+		int rowCount=0;
+		try {
+			con=dataSource.getConnection();
+			pstmt= con.prepareStatement(ProductSQL.PRODUCT_INSERT);
+			pstmt.setInt(1, product.getP_no());
+			pstmt.setString(2, product.getP_name());
+			pstmt.setInt(3, product.getP_price());
+			pstmt.setString(4, product.getP_image());
+			pstmt.setString(5, product.getP_desc());
+			pstmt.setInt(6, product.getP_click_count());
+			pstmt.setInt(7, product.getCategory_no());
+			rowCount = pstmt.executeUpdate();
+		}finally {
+			if (pstmt != null)
+				pstmt.close();
+			if (con != null)
+				con.close();
+		}
 		
 		return rowCount;
 	}
@@ -70,19 +87,26 @@ public class ProductDao {
 	 * 상품 update
 	 */
 	public int update(Product product) throws Exception {
-		Connection con = dataSource.getConnection();
-		PreparedStatement pstmt = con.prepareStatement(ProductSQL.PRODUCT_UPDATE);
-		pstmt.setString(1, product.getP_name());
-		pstmt.setInt(2, product.getP_price());
-		pstmt.setString(3, product.getP_image());
-		pstmt.setString(4, product.getP_desc());
-		pstmt.setInt(5, product.getP_click_count());
-		pstmt.setInt(6, product.getCategory_no());
-		pstmt.setInt(7,product.getP_no());
-		
-		int rowCount = pstmt.executeUpdate();
-		pstmt.close();
-		con.close();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int rowCount=0;
+		try {
+			con=dataSource.getConnection();
+			pstmt= con.prepareStatement(ProductSQL.PRODUCT_UPDATE);
+			pstmt.setString(1, product.getP_name());
+			pstmt.setInt(2, product.getP_price());
+			pstmt.setString(3, product.getP_image());
+			pstmt.setString(4, product.getP_desc());
+			pstmt.setInt(5, product.getP_click_count());
+			pstmt.setInt(6, product.getCategory_no());
+			pstmt.setInt(7,product.getP_no());
+			rowCount = pstmt.executeUpdate();
+		}finally {
+			if (pstmt != null)
+				pstmt.close();
+			if (con != null)
+				con.close();
+		}
 		
 		return rowCount;
 	}
@@ -90,12 +114,20 @@ public class ProductDao {
 	 * 상품이름으로삭제
 	 */
 	public int delete(String p_name )throws Exception{
-		Connection con = dataSource.getConnection();
-		PreparedStatement pstmt = con.prepareStatement(ProductSQL.PRODUCT_DELETE);
-		pstmt.setString(1, p_name);
-		int rowCount = pstmt.executeUpdate();
-		pstmt.close();
-		con.close();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int rowCount=0;
+		try {
+			con=dataSource.getConnection();
+			pstmt=con.prepareStatement(ProductSQL.PRODUCT_DELETE);
+			pstmt.setString(1, p_name);
+			rowCount = pstmt.executeUpdate();
+		}finally {
+			if (pstmt != null)
+				pstmt.close();
+			if (con != null)
+				con.close();
+		}
 		return rowCount;
 	}
 	/*
@@ -103,22 +135,31 @@ public class ProductDao {
 	 */
 	public Product selectByNo(int p_no) throws Exception{
 		Product product=null;
-		Connection con=dataSource.getConnection();
-		PreparedStatement pstmt=con.prepareStatement(ProductSQL.PRODUCT_SELECT_BY_NO);
-		pstmt.setInt(1, p_no);
-		ResultSet rs=pstmt.executeQuery();
-		if(rs.next()) {
-			product = new Product(rs.getInt("p_no"),
-								  rs.getString("p_name"),
-								  rs.getInt("P_price"),
-								  rs.getString("p_image"),
-								  rs.getString("p_desc"),
-								  rs.getInt("p_click_count"),
-								  rs.getInt("category_no"));
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try{
+			con=dataSource.getConnection();
+			pstmt=con.prepareStatement(ProductSQL.PRODUCT_SELECT_BY_NO);
+			pstmt.setInt(1, p_no);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				product = new Product(rs.getInt("p_no"),
+									  rs.getString("p_name"),
+									  rs.getInt("P_price"),
+									  rs.getString("p_image"),
+									  rs.getString("p_desc"),
+									  rs.getInt("p_click_count"),
+									  rs.getInt("category_no"));
+			}
+		}finally {
+			if (rs != null)
+				rs.close();
+			if (pstmt != null)
+				pstmt.close();
+			if (con != null)
+				con.close();
 		}
-		rs.close();
-		pstmt.close();
-		con.close();
 		return product;
 	}
 	/*
@@ -127,23 +168,32 @@ public class ProductDao {
 	public List<Product> selectAll() throws Exception{
 		List<Product> productList=new ArrayList<Product>();
 		
-		Connection con=dataSource.getConnection();
-		PreparedStatement pstmt=con.prepareStatement(ProductSQL.PRODUCT_SELECT_ALL);
-		ResultSet rs=pstmt.executeQuery();
-		while(rs.next()) {
-			Product product = new Product(
-					  rs.getInt("p_no"),
-					  rs.getString("p_name"),
-					  rs.getInt("p_price"),
-					  rs.getString("p_image"),
-					  rs.getString("p_desc"),
-					  rs.getInt("p_click_count"),
-					  rs.getInt("category_no"));
-			productList.add(product);
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try {
+			con=dataSource.getConnection();
+			pstmt=con.prepareStatement(ProductSQL.PRODUCT_SELECT_ALL);
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+				Product product = new Product(
+						  rs.getInt("p_no"),
+						  rs.getString("p_name"),
+						  rs.getInt("p_price"),
+						  rs.getString("p_image"),
+						  rs.getString("p_desc"),
+						  rs.getInt("p_click_count"),
+						  rs.getInt("category_no"));
+				productList.add(product);
+			}
+		}finally {
+			if (rs != null)
+				rs.close();
+			if (pstmt != null)
+				pstmt.close();
+			if (con != null)
+				con.close();
 		}
-		rs.close();
-		pstmt.close();
-		con.close();
 		return productList;
 	}
 }
